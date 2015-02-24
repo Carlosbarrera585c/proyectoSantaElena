957914127a8b49d8ac86692a6a7254e906c2f888
CARLOS BARRERA <carlosbarrera585@hotmail.com>
2015-02-23 20:01:31 -0500

    Actualizacion de indexTemplate.html y el editTemplate.html
    1.Se actualiza la parte visual de la tabla, añadiendo la clase de "table-hover".
    2.Se Actualiza la parte visual.

--- view/default/editTemplate.html.php
+++ view/default/editTemplate.html.php
@@ -2,5 +2,7 @@
 <?php use mvc\i18n\i18nClass as i18n ?>
 <?php use mvc\view\viewClass as view ?>
 <?php $usuario = usuarioTableClass::USER ?>
-<h1>EDITAR USUARIO <?php echo $objUsuario[0]->$usuario ?></h1>
+<pre>
+<h1>Editar Usuario <?php echo $objUsuario[0]->$usuario ?></h1>
 <?php view::includePartial('default/formUser', array('objUsuario' => $objUsuario, 'usuario' => $usuario)) ?>
+</pre>
