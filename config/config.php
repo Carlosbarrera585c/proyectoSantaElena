c033acaa67b406fbf8323a35df6b9aed91c7166e
ALEXANDER <ALEXANDER@alex>
2015-03-28 10:59:31 -0500

    implementacion de filtros en repoorte en la  tabla insumo y tipo insummo

--- config/config.php
+++ config/config.php
@@ -1,7 +1,10 @@
 <?php
+
 use mvc\config\configClass as config;
 use mvc\session\sessionClass as session;
+
 config::setRowGrid(3);
+
 config::setDbHost('localhost');
 config::setDbDriver('pgsql'); // pgsql
 config::setDbName('santa_helena');
@@ -10,6 +13,7 @@ config::setDbUser('postgres');
 config::setDbPassword('sena');
 // Esto solo es necesario en caso de necesitar un socket para la DB
 config::setDbUnixSocket(null);
+
 if (config::getDbUnixSocket() !== null) {
   config::setDbDsn(
           config::getDbDriver()
@@ -24,21 +28,28 @@ if (config::getDbUnixSocket() !== null) {
           . ';dbname=' . config::getDbName()
   );
 }
+
 //Para Windows
 //config::setPathAbsolute('/xampp/htdocs/proyectoSantaHelena/');
 //Para Linux 
 config::setPathAbsolute('/var/www/html/proyectoSantaHelena/');
+
 //config::setUrlBase('http://localhost/proyectoSantaHelena/web/');
+
 //Host Virtual
-config::setUrlBase('http://www.santahelena.com/');
+config::setUrlBase('http://www.santahelena.com/web/');
+
 config::setScope('prod'); // prod
 if (session::getInstance()->hasDefaultCulture() === false) {
   config::setDefaultCulture('es');
 } else {
   config::setDefaultCulture(session::getInstance()->getDefaultCulture());
 }
+
 config::setIndexFile('index.php');
+
 config::setFormatTimestamp('Y-m-d H:i:s');
+
 config::setHeaderJson('Content-Type: application/json; charset=utf-8');
 config::setHeaderXml('Content-Type: application/xml; charset=utf-8');
 config::setHeaderHtml('Content-Type: text/html; charset=utf-8');
@@ -46,19 +57,23 @@ config::setHeaderPdf('Content-type: application/pdf; charset=utf-8');
 config::setHeaderJavascript('Content-Type: text/javascript; charset=utf-8');
 config::setHeaderExcel2003('Content-Type: application/vnd.ms-excel; charset=utf-8');
 config::setHeaderExcel2007('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet; charset=utf-8');
+
 config::setCookieNameRememberMe('mvcSiteRememberMe');
 config::setCookieNameSite('mvcSite');
 config::setCookiePath('/proyectoSantaHelena/web/' . config::getIndexFile());
 //Virtual Host
-config::setCookiePath('www.santahelena.com/' . config::getIndexFile());
+config::setCookiePath('/www.santahelena.com/web/' . config::getIndexFile());
 //config::setCookieDomain('http://localhost/');
 //Virtual Host
 config::setCookieDomain('http://www.santahelena.com/');
 //config::setCookieDomain('http://localhost/');
 config::setCookieTime(3600 * 8); // una hora en segundo 3600 y por 8 serÃ­an 8 horas
+
 config::setDefaultModule('default');
 config::setDefaultAction('index');
+
 config::setDefaultModuleSecurity('shfSecurity');
 config::setDefaultActionSecurity('index');
+
 config::setDefaultModulePermission('shfSecurity');
 config::setDefaultActionPermission('noPermission');
