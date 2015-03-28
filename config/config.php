1d3529a8dcc739e6ada1e3df9564af580b53baa3
bayron45 <baironhenao82@gmail.com>
2015-03-28 15:03:09 -0500

    implementcion de bitacora

--- config/config.php
+++ config/config.php
@@ -25,12 +25,12 @@ if (config::getDbUnixSocket() !== null) {
   );
 }
 //Para Windows
-//config::setPathAbsolute('/xampp/htdocs/proyectoSantaHelena/');
+config::setPathAbsolute('/xampp/htdocs/proyectoSantaHelena/');
 //Para Linux 
-config::setPathAbsolute('/var/www/html/proyectoSantaHelena/');
-//config::setUrlBase('http://localhost/proyectoSantaHelena/web/');
+//config::setPathAbsolute('/var/www/html/proyectoSantaHelena/');
+config::setUrlBase('http://localhost/proyectoSantaHelena/web/');
 //Host Virtual
-config::setUrlBase('http://www.santahelena.com/');
+//config::setUrlBase('http://www.santahelena.com/');
 config::setScope('prod'); // prod
 if (session::getInstance()->hasDefaultCulture() === false) {
   config::setDefaultCulture('es');
@@ -50,11 +50,11 @@ config::setCookieNameRememberMe('mvcSiteRememberMe');
 config::setCookieNameSite('mvcSite');
 config::setCookiePath('/proyectoSantaHelena/web/' . config::getIndexFile());
 //Virtual Host
-config::setCookiePath('www.santahelena.com/' . config::getIndexFile());
-//config::setCookieDomain('http://localhost/');
+//config::setCookiePath('www.santahelena.com/' . config::getIndexFile());
+config::setCookieDomain('http://localhost/');
 //Virtual Host
-config::setCookieDomain('http://www.santahelena.com/');
-//config::setCookieDomain('http://localhost/');
+//config::setCookieDomain('http://www.santahelena.com/');
+config::setCookieDomain('http://localhost/');
 config::setCookieTime(3600 * 8); // una hora en segundo 3600 y por 8 serÃ­an 8 horas
 config::setDefaultModule('default');
 config::setDefaultAction('index');
