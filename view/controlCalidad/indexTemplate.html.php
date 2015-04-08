7d6ea460227cba42dcfefac9e93c47f39aed4036
bayron45 <baironhenao82@gmail.com>
2015-04-07 21:48:15 -0500

    adicion de atributos visuales en tabla controlCalidad.

--- view/controlCalidad/indexTemplate.html.php
+++ view/controlCalidad/indexTemplate.html.php
@@ -184,7 +184,7 @@ use mvc\view\viewClass as view ?>
       <a class="btn btn-warning btn-xs" data-toggle="modal" data-target="#myModalFILTROSREPORTE" ><?php echo i18n::__('printReport') ?></a>
     </div>
     <?php view::includeHandlerMessage() ?>
-    <table class="tablaUsuario table table-bordered table-responsive table-hover">
+    <table class="tablaUsuario table table-bordered table-responsive table-hover tables">
       <thead>
         <tr class="columna success">
           <th class="tamano"><input type="checkbox" id="chkAll"></th>
