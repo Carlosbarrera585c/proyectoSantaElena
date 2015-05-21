36dd42882f52f5e8b4cb31f449040d17db8adf07
Carlosbarrera585c <carlosbarrera585@hotmail.com>
2015-05-20 19:36:42 -0500

    Ajuste en El Config, algunos errores al iniciar.

--- config/config.php
+++ config/config.php
@@ -1,19 +1,15 @@
 <?php
-
 use mvc\config\configClass as config;
 use mvc\session\sessionClass as session;
-
 config::setRowGrid(4);
-
 config::setDbHost('localhost');
 config::setDbDriver('pgsql'); // mysql
-config::setDbName('santa_helena1');
+config::setDbName('santa_helena');
 config::setDbPort(5432); // 5432
 config::setDbUser('postgres');
 config::setDbPassword('sena');
 // Esto solo es necesario en caso de necesitar un socket para la DB
 config::setDbUnixSocket(null);
-
 if (config::getDbUnixSocket() !== null) {
   config::setDbDsn(
           config::getDbDriver()
@@ -28,28 +24,22 @@ if (config::getDbUnixSocket() !== null) {
           . ';dbname=' . config::getDbName()
   );
 }
-
 //Para Windows
-config::setPathAbsolute('/var/www/html/proyectoSantaHelena/');
+config::setPathAbsolute('/xampp/htdocs/proyectoSantaHelena/');
 //Para Linux 
 //config::setPathAbsolute('/var/www/html/proyectoSantaHelena/');
-
-config::setUrlBase('http://www.santahelena.com/');
-
+//Windows
+config::setUrlBase('http://localhost/proyectoSantaHelena/web/');
 //Host Virtual
 //config::setUrlBase('http://www.santahelena.com/web/');
-
 config::setScope('prod'); // prod
 if (session::getInstance()->hasDefaultCulture() === false) {
   config::setDefaultCulture('es');
 } else {
   config::setDefaultCulture(session::getInstance()->getDefaultCulture());
 }
-
 config::setIndexFile('index.php');
-
 config::setFormatTimestamp('Y-m-d H:i:s');
-
 config::setHeaderJson('Content-Type: application/json; charset=utf-8');
 config::setHeaderXml('Content-Type: application/xml; charset=utf-8');
 config::setHeaderHtml('Content-Type: text/html; charset=utf-8');
@@ -57,23 +47,20 @@ config::setHeaderPdf('Content-type: application/pdf; charset=utf-8');
 config::setHeaderJavascript('Content-Type: text/javascript; charset=utf-8');
 config::setHeaderExcel2003('Content-Type: application/vnd.ms-excel; charset=utf-8');
 config::setHeaderExcel2007('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet; charset=utf-8');
-
 config::setCookieNameRememberMe('mvcSiteRememberMe');
 config::setCookieNameSite('mvcSite');
-config::setCookiePath('/www.santahelena.com/web/' . config::getIndexFile());
 //Virtual Host
 //config::setCookiePath('/www.santahelena.com/web/' . config::getIndexFile());
-config::setCookieDomain('http://localhost/');
-//Virtual Host
+//Windows
+config::setCookiePath('/proyectoSantaHelena/web/' . config::getIndexFile());
+////Virtual Host
 //config::setCookieDomain('http://www.santahelena.com/');
-config::setCookieDomain('http://www.santahelena.com/');
+//Windows
+config::setCookieDomain('http://localhost/');
 config::setCookieTime(3600 * 8); // una hora en segundo 3600 y por 8 serÃ­an 8 horas
-
 config::setDefaultModule('usuario');
 config::setDefaultAction('index');
-
 config::setDefaultModuleSecurity('shfSecurity');
 config::setDefaultActionSecurity('index');
-
 config::setDefaultModulePermission('shfSecurity');
 config::setDefaultActionPermission('noPermission');
