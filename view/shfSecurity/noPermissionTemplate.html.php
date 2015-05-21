1190ad0b47ef45cd85d3aa3bd791c168533dadce
Carlosbarrera585c <carlosbarrera585@hotmail.com>
2015-05-21 09:18:40 -0500

    Ajuste De Mensaje Cuando el Usuario no TIene Permisos para ingresar.

--- view/shfSecurity/noPermissionTemplate.html.php
+++ view/shfSecurity/noPermissionTemplate.html.php
@@ -1,1 +1,13 @@
-usted no tiene permisos para entrar aquí
+<?php
+
+use mvc\routing\routingClass as routing ?>
+<?php use mvc\view\viewClass as view ?>
+<div class="container container-fluid">
+    <form class="form-signin">
+        <div class="alert alert-danger">
+            <h3 class="form-signin-heading">Usted No Tiene Permisos Para Entrar Aquí.</h3>
+            <a href="<?php echo routing::getInstance()->getUrlWeb('shfSecurity', 'logout') ?>" class="alert-link">Presione aqui Para Volver.</a>
+
+        </div>
+    </form>
+</div>
