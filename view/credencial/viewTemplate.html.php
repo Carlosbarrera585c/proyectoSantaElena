cf7865c74485e6bb3819c07a57634f04861b36a8
carlosbarrera <carlosbarrera@carlosbarrera-HP-Pavilion-dm1-Notebook-PC>
2015-07-09 15:57:04 -0500

    Implementacion de Restricciones de Usuarios. 1. Se Implementa en las tablas: Empleado, Credencial, Usuario. 2. Se Ocultan los Botones de Eliminar, Eliminar Masivo, Editar. 3. Restriccion al Usuario 'Carlos', quien no podra ver las Acciones.

--- view/credencial/viewTemplate.html.php
+++ view/credencial/viewTemplate.html.php
@@ -5,6 +5,7 @@ use mvc\routing\routingClass as routing ?>
 use mvc\i18n\i18nClass as i18n ?>
 <?php
 use mvc\view\viewClass as view ?>
+<?php use mvc\session\sessionClass as session?>
 <?php view::includePartial('menu/menu') ?>
 <?php $id = credencialTableClass::ID ?>
 <?php $nom = credencialTableClass::NOMBRE ?>
@@ -16,7 +17,9 @@ use mvc\view\viewClass as view ?>
   </div>
   <form id="frmDeleteAll" action="<?php echo routing::getInstance()->getUrlWeb('credencial', 'deleteSelect') ?>" method="POST">
     <div style="margin-bottom: 10px; margin-top: 30px">
+        <?php if (session::getInstance()->hasCredential('admin')): ?>
       <a href="<?php echo routing::getInstance()->getUrlWeb('credencial', 'insert') ?>" class="btn btn-success btn-xs"><?php echo i18n::__('new') ?></a>
+      <?php endif; ?>
       <a href="<?php echo routing::getInstance()->getUrlWeb('credencial', 'index') ?>" class="btn btn-info btn-xs"><?php echo i18n::__('back') ?></a>
     </div>
     <table class="table table-bordered table-responsive table-condensed tables">
