79fe3577b633c6493bd3b40f8ef041e51b88fbd8
Cristian <Cristian@Gamer>
2015-07-24 10:39:04 +0200

    detalle salida Bodega: EDITAR Y ELIMINAR MAS BD ACTUALIZADA

--- view/detalleSalida/editTemplate.html.php
+++ view/detalleSalida/editTemplate.html.php
@@ -1,10 +1,13 @@
 <?php use mvc\routing\routingClass as routing ?>
 <?php use mvc\i18n\i18nClass as i18n ?>
 <?php use mvc\view\viewClass as view ?>
-<?php $id = detalleEntradaTableClass::ID ?>
+<?php $id = detalleSalidaTableClass::ID ?>
+<?php $cantidad = detalleSalidaTableClass::CANTIDAD ?>
 <?php view::includePartial('menu/menu') ?>
  <div class="page-header  text-center titulo">
         <h1><i class="glyphicon glyphicon-user"> <?php echo i18n::__('editDetailEntrance') ?></i></h1>
-        <?php echo $objDetalleEntrada[0]->$id ?>
+        <?php echo $objDetalleSalida[0]->$id ?>
     </div>
-<?php view::includePartial('detalleEntrada/formEntrada', array('objDetalleEntrada' => $objDetalleEntrada, 'detalleEntrada' => $id, 'objTipoDoc' => $objTipoDoc, 'objEntradaBodega' => $objEntradaBodega, 'objInsu' => $objInsu)) ?>
+<?php view::includePartial('detalleSalida/formSalida', array('objDetalleSalida' => $objDetalleSalida, 'detalleSalida' => $id, 'objTipoDoc' => $objTipoDoc, 'objSalidaBodega' => $objSalidaBodega, 'objInsu' => $objInsu)) ?>
+
+
