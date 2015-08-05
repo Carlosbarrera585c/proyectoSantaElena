492c724879d4ed263a7c863eae3842b7d8710580
carlosbarrera <carlosbarrera@carlosbarrera-HP-Pavilion-dm1-Notebook-PC>
2015-08-05 01:53:56 -0500

    Ajustes de la funcion editar en la tabla empleado, sus validaciones pertinentes.

--- controller/empleado/updateActionClass.php
+++ controller/empleado/updateActionClass.php
@@ -59,99 +59,107 @@ class updateActionClass extends controllerClass implements controllerActionInter
     }
 
     private function Validate($nomEmpleado, $apellEmpleado, $telefono, $direccion, $tipoId, $numeroIdentificacion, $credencialId, $correo, $correo2) {
-        $bandera = false;
+        $bandera = FALSE;
         if (strlen($nomEmpleado) > empleadoTableClass::NOM_EMPLEADO_LENGTH) {
-            session::getInstance()->setError(i18n::__('errorLengthEmployee', NULL, 'default', array('%nombre%' => $nomEmpleado, '%caracteres%' => empleadoTableClass::NOM_EMPLEADO_LENGTH)));
+            session::getInstance()->setError(i18n::__('errorLengthEmployee', NULL, 'default', array('%nombre%' => $nomEmpleado, '%caracteres%' => empleadoTableClass::NOM_EMPLEADO_LENGTH)), 'errorNombre');
             $bandera = true;
             session::getInstance()->setFlash(empleadoTableClass::getNameField(empleadoTableClass::NOM_EMPLEADO, true), true);
         } elseif (!preg_match('/^[a-zA-Z ]*$/', $nomEmpleado)) {
-            session::getInstance()->setError(i18n::__('errorText', NULL, 'default', array('%texto%' => $nomEmpleado)));
+            session::getInstance()->setError(i18n::__('errorText', NULL, 'default', array('%texto%' => $nomEmpleado)), 'errorNombre');
             $bandera = true;
             session::getInstance()->setFlash(empleadoTableClass::getNameField(empleadoTableClass::NOM_EMPLEADO, true), true);
-        } elseif ($nomEmpleado === '') {
-            session::getInstance()->setError(i18n::__('errorNull', NULL, 'default'));
+        } elseif ($nomEmpleado === NULL) {
+            session::getInstance()->setError(i18n::__('errorNull', NULL, 'default'), 'errorNombre');
             $bandera = true;
             session::getInstance()->setFlash(empleadoTableClass::getNameField(empleadoTableClass::NOM_EMPLEADO, true), true);
         }
         if (strlen($apellEmpleado) > empleadoTableClass::APELL_EMPLEADO_LENGTH) {
-            session::getInstance()->setError(i18n::__('errorLengthLastEmployee', NULL, 'default', array('%apellido%' => $apellEmpleado, '%caracteres%' => empleadoTableClass::APELL_EMPLEADO_LENGTH)));
+            session::getInstance()->setError(i18n::__('errorLengthLastEmployee', NULL, 'default', array('%apellido%' => $apellEmpleado, '%caracteres%' => empleadoTableClass::APELL_EMPLEADO_LENGTH)), 'errorApellido');
             $bandera = true;
             session::getInstance()->setFlash(empleadoTableClass::getNameField(empleadoTableClass::APELL_EMPLEADO, true), true);
         } elseif (!preg_match('/^[a-zA-Z ]*$/', $apellEmpleado)) {
-            session::getInstance()->setError(i18n::__('errorText', NULL, 'default', array('%texto%' => $apellEmpleado)));
+            session::getInstance()->setError(i18n::__('errorText', NULL, 'default', array('%texto%' => $apellEmpleado)), 'errorApellido');
             $bandera = true;
             session::getInstance()->setFlash(empleadoTableClass::getNameField(empleadoTableClass::APELL_EMPLEADO, true), true);
-        } elseif ($apellEmpleado === '') {
-            session::getInstance()->setError(i18n::__('errorNull', NULL, 'default'));
+        } elseif ($apellEmpleado === NULL) {
+            session::getInstance()->setError(i18n::__('errorNull', NULL, 'default'), 'errorApellido');
             $bandera = true;
             session::getInstance()->setFlash(empleadoTableClass::getNameField(empleadoTableClass::APELL_EMPLEADO, true), true);
         }
-        if ($telefono > empleadoTableClass::TELEFONO) {
-            session::getInstance()->setError(i18n::__('errorLengthPhone', NULL, 'default', array('%telefono%' => $telefono, '%caracteres%' => empleadoTableClass::TELEFONO_LENGTH)));
+        if (strlen($telefono) > empleadoTableClass::TELEFONO_LENGTH) {
+            session::getInstance()->setError(i18n::__('errorLengthPhone', NULL, 'default', array('%telefono%' => $telefono, '%caracteres%' => empleadoTableClass::TELEFONO_LENGTH)), 'errorTelefono');
             $bandera = true;
             session::getInstance()->setFlash(empleadoTableClass::getNameField(empleadoTableClass::TELEFONO, true), true);
         } elseif (!is_numeric($telefono)) {
-            session::getInstance()->setError(i18n::__('errorNumeric', NULL, 'default'));
+            session::getInstance()->setError(i18n::__('errorNumeric', NULL, 'default'), 'errorTelefono');
             $bandera = true;
             session::getInstance()->setFlash(empleadoTableClass::getNameField(empleadoTableClass::TELEFONO, true), true);
-        } elseif ($telefono === '') {
-            session::getInstance()->setError(i18n::__('errorNull', NULL, 'default'));
+        } elseif ($telefono === NULL) {
+            session::getInstance()->setError(i18n::__('errorNull', NULL, 'default'), 'errorTelefono');
             $bandera = true;
             session::getInstance()->setFlash(empleadoTableClass::getNameField(empleadoTableClass::TELEFONO, true), true);
         }
         if (strlen($direccion) > empleadoTableClass::DIRECCION_LENGTH) {
-            session::getInstance()->setError(i18n::__('errorLengthDirection', NULL, 'default', array('%direccion%' => $direccion, '%caracteres%' => empleadoTableClass::DIRECCION_LENGTH)));
+            session::getInstance()->setError(i18n::__('errorLengthDirection', NULL, 'default', array('%direccion%' => $direccion, '%caracteres%' => empleadoTableClass::DIRECCION_LENGTH)), 'errorDireccion');
             $bandera = true;
             session::getInstance()->setFlash(empleadoTableClass::getNameField(empleadoTableClass::DIRECCION, true), true);
-        } elseif ($direccion === '') {
-            session::getInstance()->setError(i18n::__('errorNull', NULL, 'default'));
+        } elseif ($direccion === NULL) {
+            session::getInstance()->setError(i18n::__('errorNull', NULL, 'default'), 'errorDireccion');
             $bandera = true;
             session::getInstance()->setFlash(empleadoTableClass::getNameField(empleadoTableClass::DIRECCION, true), true);
         }
         if (strlen($numeroIdentificacion) > empleadoTableClass::NUMERO_IDENTIFICACION_LENGTH) {
-            session::getInstance()->setError(i18n::__('errorLengthNumIdentification', NULL, 'default', array('%numIdentification%' => $numeroIdentificacion, '%caracteres%' => empleadoTableClass::NUMERO_IDENTIFICACION_LENGTH)));
+            session::getInstance()->setError(i18n::__('errorLengthNumIdentification', NULL, 'default', array('%numIdentification%' => $numeroIdentificacion, '%caracteres%' => empleadoTableClass::NUMERO_IDENTIFICACION_LENGTH)), 'errorNumeroIdentificacion');
             $bandera = true;
             session::getInstance()->setFlash(empleadoTableClass::getNameField(empleadoTableClass::NUMERO_IDENTIFICACION, true), true);
         } elseif (!is_numeric($numeroIdentificacion)) {
-            session::getInstance()->setError(i18n::__('errorNumeric', NULL, 'default'));
+            session::getInstance()->setError(i18n::__('errorNumeric', NULL, 'default'), 'errorNumeroIdentificacion');
             $bandera = true;
             session::getInstance()->setFlash(empleadoTableClass::getNameField(empleadoTableClass::NUMERO_IDENTIFICACION, true), true);
-        } elseif ($numeroIdentificacion === '') {
-            session::getInstance()->setError(i18n::__('errorNull', NULL, 'default'));
+        } elseif ($numeroIdentificacion === NULL) {
+            session::getInstance()->setError(i18n::__('errorNull', NULL, 'default'), 'errorNumeroIdentificacion');
             $bandera = true;
             session::getInstance()->setFlash(empleadoTableClass::getNameField(empleadoTableClass::NUMERO_IDENTIFICACION, true), true);
         }
         if ($correo !== $correo2) {
-            session::getInstance()->setError(i18n::__('errorMail', NULL, 'default'));
+            session::getInstance()->setError(i18n::__('errorMail', NULL, 'default'), 'errorCorreo');
+            $bandera = true;
+            session::getInstance()->setFlash(empleadoTableClass::getNameField(empleadoTableClass::CORREO, true), true);
+        } elseif (!filter_var($correo, FILTER_VALIDATE_EMAIL)) {
+            session::getInstance()->setError(i18n::__('errorMailCharacters', NULL, 'default'), 'errorCorreo2');
+            $bandera = true;
+            session::getInstance()->setFlash(empleadoTableClass::getNameField(empleadoTableClass::CORREO, true), true);
+        } elseif ($correo === NULL) {
+            session::getInstance()->setError(i18n::__('errorNull', NULL, 'default'), 'errorCorreo');
             $bandera = true;
             session::getInstance()->setFlash(empleadoTableClass::getNameField(empleadoTableClass::CORREO, true), true);
         }
-        if (filter_var($correo, FILTER_VALIDATE_EMAIL)) {
-            session::getInstance()->setError(i18n::__('errorMailCharacters', NULL, 'default'));
-            $bandera = true;
-            session::getInstance()->setFlash(empleadoTableClass::getNameField(empleadoTableClass::CORREO, true), true);
-        } elseif ($correo === '') {
-            session::getInstance()->setError(i18n::__('errorNull', NULL, 'default'));
-            $bandera = true;
-            session::getInstance()->setFlash(empleadoTableClass::getNameField(empleadoTableClass::CORREO, true), true);
-        } elseif ($correo2 === '') {
-            session::getInstance()->setError(i18n::__('errorNull', NULL, 'default'));
+        if ($correo2 === NULL) {
+            session::getInstance()->setError(i18n::__('errorNull', NULL, 'default'), 'errorCorreo');
             $bandera = true;
             session::getInstance()->setFlash(empleadoTableClass::getNameField(empleadoTableClass::CORREO, true), true);
         }
-        if ($tipoId === '') {
-            session::getInstance()->setError(i18n::__('errorNull', NULL, 'default'));
+        if ($tipoId === NULL) {
+            session::getInstance()->setError(i18n::__('errorNull', NULL, 'default'), 'errorTipoId');
+            $bandera = true;
+            session::getInstance()->setFlash(empleadoTableClass::getNameField(empleadoTableClass::TIPO_ID_ID, true), true);
+        } elseif (!is_numeric($tipoId)) {
+            session::getInstance()->setError(i18n::__('errorNumeric', NULL, 'default'), 'errorTipoId');
             $bandera = true;
             session::getInstance()->setFlash(empleadoTableClass::getNameField(empleadoTableClass::TIPO_ID_ID, true), true);
         }
-        if ($credencialId === '') {
-            session::getInstance()->setError(i18n::__('errorNull', NULL, 'default'));
+        if ($credencialId === NULL) {
+            session::getInstance()->setError(i18n::__('errorNull', NULL, 'default'), 'errorCredencial');
+            $bandera = true;
+            session::getInstance()->setFlash(empleadoTableClass::getNameField(empleadoTableClass::CREDENCIAL_ID, true), true);
+        } elseif (!is_numeric($credencialId)) {
+            session::getInstance()->setError(i18n::__('errorNumeric', NULL, 'default'), 'errorCredencial');
             $bandera = true;
             session::getInstance()->setFlash(empleadoTableClass::getNameField(empleadoTableClass::CREDENCIAL_ID, true), true);
         }
         if ($bandera === true) {
             request::getInstance()->setMethod('GET');
-            request::getInstance()->addParamGet(array(empleadoTableClass::ID => request::getInstance()->getPost(empleadoTableClass::getNameField(empleadoTableClass::ID, true))));
+            request::getInstance()->addParamGet(array(empleadoTableClass::ID => request::getInstance()->getPost(empleadoTableClass::getNameField(empleadoTableClass::ID, TRUE))));
             routing::getInstance()->forward('empleado', 'edit');
         }
     }
