ddab3661086b5d38a4f11e4e57cacd272df32cc0
carlo <carlo@DESKTOP-C86S63I>
2015-08-12 01:32:43 -0500

    Ajustes en la Plantilla del Instalador.

--- installer/plantilla.php
+++ installer/plantilla.php
@@ -1,11 +1,8 @@
 <?php
 $config = "<?php
-
 use mvc\config\configClass as config;
 use mvc\session\sessionClass as session;
-
 config::setRowGrid($RowGrid);
-
 config::setDbHost('$host');
 config::setDbDriver('$driver'); // mysql
 config::setDbName('$dbName');
@@ -14,7 +11,6 @@ config::setDbUser('$dbUser');
 config::setDbPassword('$dbPass');
 // Esto solo es necesario en caso de necesitar un socket para la DB
 config::setDbUnixSocket(null);
-
 if (config::getDbUnixSocket() !== null) {
   config::setDbDsn(
           config::getDbDriver()
@@ -29,28 +25,16 @@ if (config::getDbUnixSocket() !== null) {
           . ';dbname=' . config::getDbName()
   );
 }
-
-//Para Windows
 config::setPathAbsolute('$PathAbsolute');
-//Para Linux 
-//config::setPathAbsolute('/var/www/html/proyectoSantaHelena/');
-
 config::setUrlBase('$UrlBase');
-
-//Host Virtual
-//config::setUrlBase('http://www.santahelena.com/web/');
-
-config::setScope('$Scope'); // prod
+config::setScope('$Scope'); // dev
 if (session::getInstance()->hasDefaultCulture() === false) {
   config::setDefaultCulture('$idioma');
 } else {
   config::setDefaultCulture(session::getInstance()->getDefaultCulture());
 }
-
 config::setIndexFile('index.php');
-
 config::setFormatTimestamp('$FormatTimestamp');
-
 config::setHeaderJson('Content-Type: application/json; charset=utf-8');
 config::setHeaderXml('Content-Type: application/xml; charset=utf-8');
 config::setHeaderHtml('Content-Type: text/html; charset=utf-8');
@@ -58,23 +42,14 @@ config::setHeaderPdf('Content-type: application/pdf; charset=utf-8');
 config::setHeaderJavascript('Content-Type: text/javascript; charset=utf-8');
 config::setHeaderExcel2003('Content-Type: application/vnd.ms-excel; charset=utf-8');
 config::setHeaderExcel2007('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet; charset=utf-8');
-
 config::setCookieNameRememberMe('mvcSiteRememberMe');
 config::setCookieNameSite('mvcSite');
 config::setCookiePath('$cookiePath' . config::getIndexFile());
-//Virtual Host
-//config::setCookiePath('/www.santahelena.com/web/' . config::getIndexFile());
-config::setCookieDomain('http://localhost/');
-//Virtual Host
-//config::setCookieDomain('http://www.santahelena.com/');
 config::setCookieDomain('$UrlBase');
 config::setCookieTime(3600 * 8); // una hora en segundo 3600 y por 8 serÃ­an 8 horas
-
 config::setDefaultModule('usuario');
 config::setDefaultAction('index');
-
 config::setDefaultModuleSecurity('shfSecurity');
 config::setDefaultActionSecurity('index');
-
 config::setDefaultModulePermission('shfSecurity');
 config::setDefaultActionPermission('noPermission');";
--- installer/view/index.html.php
+++ installer/view/index.html.php
@@ -1,80 +1,80 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="UTF-8">
-    <title></title>
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
-    <link rel="stylesheet" href="./css/main.css">
-  </head>
-  <body><br><br><br><br>
-      <div class="container container-fluid">
-          <div class="row">
-              <div class="page-header  text-center titulo">
-        <h1><i class="glyphicon glyphicon-th-list"> Instalador Agro Panela Santa Helena</i><br><small> Requisitos del Sistema</small></h1>
-    </div>
-      <table class="table tables table-responsive table-bordered table-striped table-hover"> 
-          <tr class="info">
-            <th>Extensiones Del Sistema</th>
-            <th>Extension En El Sistema</th>
-            <th>Si</th>
-            <th>No</th>
-        </tr>
-        <tr>
-            <td>PHP 5.4 o Superior</td>
-            <td><?php echo PHP_VERSION?></td>
-            <td><?php echo ((PHP_VERSION > 5.4) ? '<i class="glyphicon glyphicon-thumbs-up"></i>' : '') ?></td>
-            <td><?php echo ((PHP_VERSION > 5.4) ? '' : '<i class="glyphicon glyphicon-thumbs-down"></i>') ?></i></td>
-            
-        </tr>
-        <tr>
-            <td>JSON</td>
-            <td><?php echo get_loaded_extensions()[array_search('json', get_loaded_extensions())] ?></td>
-            <?php if (get_loaded_extensions()[array_search('json', get_loaded_extensions())] === 'json') : ?>
-            <td><i class="glyphicon glyphicon-thumbs-up"></i></td>
-            <td></td>
-            <?php else : ?>
-            <td></td>
-            <td><i class="glyphicon glyphicon-thumbs-down"></i></td>
-            <?php endif;?>
-            
-        </tr>
-        <tr>
-            <td>PDO</td>
-            <td><?php echo get_loaded_extensions()[array_search('PDO', get_loaded_extensions())] ?></td>
-            <?php if (get_loaded_extensions()[array_search('PDO', get_loaded_extensions())] === 'PDO') : ?>
-            <td><i class="glyphicon glyphicon-thumbs-up"></i></td>
-            <td></td>
-            <?php else : ?>
-            <td></td>
-            <td><i class="glyphicon glyphicon-thumbs-down"></i></td>
-            <?php endif;?>
-        </tr>
-        <tr>
-            <td>PDO_PGSQL</td>
-            <td><?php echo get_loaded_extensions()[array_search('pdo_pgsql', get_loaded_extensions())] ?></td>
-            <?php if (get_loaded_extensions()[array_search('pdo_pgsql', get_loaded_extensions())] === 'pdo_pgsql') : ?>
-            <td><i class="glyphicon glyphicon-thumbs-up"></i></td>
-            <td></td>
-            <?php else : ?>
-            <td></td>
-            <td><i class="glyphicon glyphicon-thumbs-down"></i></td>
-            <?php endif;?>
-        </tr>
-        <tr>
-            <td>PDO_MYSQL</td>
-            <td><?php echo get_loaded_extensions()[array_search('pdo_mysql', get_loaded_extensions())] ?></td>
-            <?php if (get_loaded_extensions()[array_search('pdo_mysql', get_loaded_extensions())] === 'pdo_mysql') : ?>
-            <td><i class="glyphicon glyphicon-thumbs-up"></i></td>
-            <td></td>
-            <?php else : ?>
-            <td></td>
-            <td><i class="glyphicon glyphicon-thumbs-down"></i></td>
-            <?php endif;?>
-        </tr>
-    </table>
-             
-              <a  href="index.php?step=2" class="btn btn-success btn-xs">Siguiente</a>
-          </div>
-      </div>
-  </body>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="./css/bootstrap.min.css">
+        <link rel="stylesheet" href="./css/main.css">
+    </head>
+    <body><br><br><br><br>
+        <div class="container container-fluid">
+            <div class="row">
+                <div class="page-header  text-center titulo">
+                    <h1><i class="glyphicon glyphicon-th-list"> Instalador del Sistema</i><br><br><small> Requisitos</small></h1>
+                </div>
+                <table class="table tables table-responsive table-bordered table-striped table-hover"> 
+                    <tr class="info">
+                        <th>Extensiones Del Sistema</th>
+                        <th>Extension En El Sistema</th>
+                        <th>Si</th>
+                        <th>No</th>
+                    </tr>
+                    <tr>
+                        <td>PHP 5.4 o Superior</td>
+                        <td><?php echo PHP_VERSION ?></td>
+                        <td><?php echo ((PHP_VERSION > 5.4) ? '<i class="glyphicon glyphicon-thumbs-up"></i>' : '') ?></td>
+                        <td><?php echo ((PHP_VERSION > 5.4) ? '' : '<i class="glyphicon glyphicon-thumbs-down"></i>') ?></i></td>
+
+                    </tr>
+                    <tr>
+                        <td>JSON</td>
+                        <td><?php echo get_loaded_extensions()[array_search('json', get_loaded_extensions())] ?></td>
+                        <?php if (get_loaded_extensions()[array_search('json', get_loaded_extensions())] === 'json') : ?>
+                            <td><i class="glyphicon glyphicon-thumbs-up"></i></td>
+                            <td></td>
+                        <?php else : ?>
+                            <td></td>
+                            <td><i class="glyphicon glyphicon-thumbs-down"></i></td>
+                        <?php endif; ?>
+
+                    </tr>
+                    <tr>
+                        <td>PDO</td>
+                        <td><?php echo get_loaded_extensions()[array_search('PDO', get_loaded_extensions())] ?></td>
+                        <?php if (get_loaded_extensions()[array_search('PDO', get_loaded_extensions())] === 'PDO') : ?>
+                            <td><i class="glyphicon glyphicon-thumbs-up"></i></td>
+                            <td></td>
+                        <?php else : ?>
+                            <td></td>
+                            <td><i class="glyphicon glyphicon-thumbs-down"></i></td>
+                        <?php endif; ?>
+                    </tr>
+                    <tr>
+                        <td>PDO_PGSQL</td>
+                        <td><?php echo get_loaded_extensions()[array_search('pdo_pgsql', get_loaded_extensions())] ?></td>
+                        <?php if (get_loaded_extensions()[array_search('pdo_pgsql', get_loaded_extensions())] === 'pdo_pgsql') : ?>
+                            <td><i class="glyphicon glyphicon-thumbs-up"></i></td>
+                            <td></td>
+                        <?php else : ?>
+                            <td></td>
+                            <td><i class="glyphicon glyphicon-thumbs-down"></i></td>
+                        <?php endif; ?>
+                    </tr>
+                    <tr>
+                        <td>PDO_MYSQL</td>
+                        <td><?php echo get_loaded_extensions()[array_search('pdo_mysql', get_loaded_extensions())] ?></td>
+                        <?php if (get_loaded_extensions()[array_search('pdo_mysql', get_loaded_extensions())] === 'pdo_mysql') : ?>
+                            <td><i class="glyphicon glyphicon-thumbs-up"></i></td>
+                            <td></td>
+                        <?php else : ?>
+                            <td></td>
+                            <td><i class="glyphicon glyphicon-thumbs-down"></i></td>
+                        <?php endif; ?>
+                    </tr>
+                </table>
+
+                <a  href="index.php?step=2" class="btn btn-success btn-xs">Siguiente</a>
+            </div>
+        </div>
+    </body>
 </html>
