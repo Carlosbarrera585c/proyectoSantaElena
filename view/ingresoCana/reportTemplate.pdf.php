7ad3bed529576aa1e034d0f2acce41e49887f4a8
Esteban Henao <Esteban Henao@Henao.home>
2015-08-27 03:01:32 -0500

    correccion de reportes pdf en control calidad e ingreso caña

--- view/ingresoCana/reportTemplate.pdf.php
+++ view/ingresoCana/reportTemplate.pdf.php
@@ -15,36 +15,30 @@ $num_vagon = ingresoCanaTableClass::NUM_VAGON;
 $pdf = new FPDF('l', 'mm', 'letter');
 $pdf->AddPage();
 $pdf->Cell(80);
-$pdf->SetFont('Arial','B',12);
-$pdf->Image(routing::getInstance()->getUrlImg('controlDeCalidad.jpg'),90,8,70);
+$pdf->SetFont('courier','B',12);
+$pdf->Image(routing::getInstance()->getUrlImg('logoProyecto.jpg'),0,0,280);
 
 $pdf ->Ln(50);
 
-$pdf->Cell(255,10,'INGRESO CAÑA',1,1,'C');
-
-
-
-
-  $pdf->Cell(10,10,  utf8_decode("ID"),1);
+$pdf->Cell (255,10,'INGRESO CANA',1,1,'C');
   $pdf->Cell(42,10,  utf8_decode("FECHA"),1);
-   $pdf->Cell(32,10,  utf8_decode("EMPLEADO_ID"),1);
-  $pdf->Cell(35,10,  utf8_decode("PROVEEDOR_ID"),1);
-  $pdf->Cell(20,10,  utf8_decode("CANTIDAD"),1);
-  $pdf->Cell(17,10,  utf8_decode("PROCEDENCIA"),1);
-  $pdf->Cell(17,10,  utf8_decode("PESO_CAÑA"),1);
-  $pdf->Cell(17,10,  utf8_decode("NUM_VAGON"),1);
+  $pdf->Cell(32,10,  utf8_decode("EMPLEADO"),1);
+  $pdf->Cell(35,10,  utf8_decode("PROVEEDOR"),1);
+  $pdf->Cell(30,10,  utf8_decode("CANTIDAD"),1);
+  $pdf->Cell(45,10,  utf8_decode("PROCEDENCIA"),1);
+  $pdf->Cell(35,10,  utf8_decode("PESO CAÑA"),1);
+  $pdf->Cell(36,10,  utf8_decode("NUM VAGON"),1);
   
  
   $pdf->Ln();
 foreach ($objingresoCana as $ingreso){
-  $pdf->Cell(10,10,  utf8_decode($ingreso->$id),1);
   $pdf->Cell(42,10,  utf8_decode($ingreso->$fecha),1);
-  $pdf->Cell(32,10,  utf8_decode($ingreso->$empleado_id),1);
-  $pdf->Cell(35,10,  utf8_decode($ingreso->$proveedor_id),1);
-  $pdf->Cell(20,10,  utf8_decode($ingreso->$cantidad),1);
-  $pdf->Cell(17,10,  utf8_decode($ingreso->$procedencia_caña),1);
-  $pdf->Cell(17,10,  utf8_decode($ingreso->$peso_caña),1);
-  $pdf->Cell(17,10,  utf8_decode($ingreso->$num_vagon),1);
+  $pdf->Cell(32,10,  utf8_decode(ingresoCanaTableClass::getNameEmpleado($ingreso->$empleado_id)),1);
+  $pdf->Cell(35,10,  utf8_decode(ingresoCanaTableClass::getNameProveedor($ingreso->$proveedor_id)),1);
+  $pdf->Cell(30,10,  utf8_decode($ingreso->$cantidad),1);
+  $pdf->Cell(45,10,  utf8_decode($ingreso->$procedencia_caña),1);
+  $pdf->Cell(35,10,  utf8_decode($ingreso->$peso_caña),1);
+  $pdf->Cell(36,10,  utf8_decode($ingreso->$num_vagon),1);
   
   
   
